7d68f2873ae2c3c851e1e49703363a3192fc90cb
8LBL1101 <kuiqinchuan88@gmail.com>
2022-07-25 18:32:52 +0900

    1,3は完成。2は未完成 ファイルの書き込みがうまくいかない 標準入力で受け取ったものが正確に表記されない。おそらく文字化け。コマンドプロンプトはsjisらしい？javaのstring型はutf16らしい

--- konndate.java
+++ konndate.java
@@ -4,27 +4,19 @@ import java.io.*;
 class kondate {
 //メインメソッド
   public static void main(String[] args){
-    /*Control ctl;
-    ctl = new Control();
+    Control ctl = new Control();
     //ctl.menue = null;
     ctl.filecheck(ctl.menue);
-    System.out.println(ctl.menue);*/
-    //System.out.println(ctl.history);
+
+    //System.out.println(ctl.history[0]);
 
     //==================
-    String menue[] = null;
-    String history[] = new String[14];
-
+    //String menue[];
+    //String history[] = new String[14];
+/*
     FileReader fr = null;
     try{
       fr = new FileReader("menue.txt");
-      Scanner scn = new Scanner(fr);
-      int i = 0;
-      while(scn.hasNext()){
-        String s = scn.nextLine();
-        menue[i] = s;
-        i++;
-      }
     }catch(IOException e){
       System.out.println("inout error");
     }catch(NumberFormatException e){
@@ -32,25 +24,36 @@ class kondate {
     }catch(Exception e){
       System.out.println("error");
     }
-
+    Scanner scn = new Scanner(fr);
+    int i = 0;
+    while(scn.hasNext()){
+      String s = scn.nextLine();
+      System.out.println(s);
+      ctl.menue[i] = s;
+      i++;
+    }
+*/
     //==================
 
     int end = 0;
     //System.out.println("a");
     loop: for(end = 0; end == 0; ){
-      System.out.println("1:献立候補一覧\n2:献立入力\n3:献立履歴一覧\n4:\n99:終了\n");
+      System.out.println("\n1:献立候補一覧\n2:今日の献立入力\n3:献立履歴一覧\n4:新規メニュー追加\n99:終了\n");
       Scanner scan = new Scanner(System.in);
       try{
         int num = scan.nextInt();
         switch(num){
           case 1:
             System.out.println(num);
+            ctl.fileall();
             break;
           case 2:
             System.out.println(num);
+            ctl.daymenue();
             break;
           case 3:
             System.out.println(num);
+            ctl.filehis();
             break;
           case 4:
             System.out.println(num);
@@ -80,7 +83,7 @@ class kondate {
 //操作クラス
 class Control{
 
-  String menue[];
+  String menue[] = new String[28];
   String history[] = new String[14];
 
   void filecheck(String[] x){
@@ -97,14 +100,89 @@ class Control{
       x[i] = s;
       i++;
     }
-    /*i=0;
+    i=0;
     while(i<14){
-      history[i] = menue[i];
+      history[i] = x[i];
       i++;
-    }*/
+    }
   }
 
   void fileall(){
-
+    System.out.println("========献立候補一覧========");
+    for(int i = menue.length; i>0;i--){
+      if(menue[i-1] != null){
+        System.out.println(menue[i-1]);
+      }
+    }
+    System.out.println("===========================");
   }
+
+  void daymenue(){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("今日の献立を入力してください");
+    String newmenue=sc.nextLine();
+    System.out.println("テスト"+newmenue);
+    int delcheck = 0;
+    int i;
+    for(i = 14; i<menue.length; i++){
+      if(newmenue == menue[i]){
+        menue[i]=null;
+        delcheck=1;
+      }
+    }
+    if(delcheck == 0){
+      for(i = menue.length; i >1;i--){
+        if(menue[i-2] != null){
+          menue[i-1]=menue[i-2];
+        }
+      }
+      menue[0]=newmenue;
+    }else{
+      for(i=0; i < menue.length; i++){
+        if(menue[i] == null){
+          break;
+        }
+      }
+      for(; i > 1; i-- ){
+        if(menue[i-2] != null){
+          menue[i-1]=menue[i-2];
+        }
+      }
+      menue[0]=newmenue;
+    }
+    //change();
+  }//daymenue end
+
+  void filehis(){
+    System.out.println("========献立履歴一覧========");
+    for(int i = 0; i<history.length; i++){
+      if(history[i] != null){
+        System.out.println(history[i]);
+      }
+    }
+    System.out.println("===========================");
+  }
+
+  void change(){
+    //ファイルの書き込み
+    String newtxt=menue[0];
+    try{
+      FileWriter fw = new FileWriter("menue.txt");
+      PrintWriter pw = new PrintWriter(new BufferedWriter(fw));
+
+      for(int i = 1;menue.length > i; i++ ){
+        if(menue[i] != null){
+          newtxt=(newtxt+"\n"+menue[i]);
+        }
+      }
+      pw.println("生米");
+      //System.out.println(newtxt);
+      pw.close();
+    }catch(IOException e){
+      e.printStackTrace();
+    }
+
+
+  }//change end
+
 }//操作クラス終了
