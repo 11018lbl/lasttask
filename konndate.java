7cd35768e6d5dfb2536a3266a8cdb7b20a2830fa
8LBL1101 <kuiqinchuan88@gmail.com>
2022-07-26 18:59:58 +0900

    どうしても解決しなかったのでmenueはローマ字表記として一時対応。ファイルの書き込みはコード書いた気になってただけで書いてなかった。かいた。解決。 検索機能の実装。ただし検索文字の入力以降のどこかでエラー。今後対応

--- konndate.java
+++ konndate.java
@@ -1,5 +1,7 @@
 import java.util.*;
 import java.io.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 class kondate {
 //メインメソッド
@@ -39,7 +41,7 @@ class kondate {
     int end = 0;
     //System.out.println("a");
     loop: for(end = 0; end == 0; ){
-      System.out.println("\n1:献立候補一覧\n2:今日の献立入力\n3:献立履歴一覧\n4:新規メニュー追加\n99:終了\n");
+      System.out.println("\n1:献立候補一覧\n2:今日の献立入力\n3:献立履歴一覧\n4:新規メニュー追加\n5:登録名検索\n99:終了\n");
       Scanner scan = new Scanner(System.in);
       try{
         int num = scan.nextInt();
@@ -58,7 +60,15 @@ class kondate {
             break;
           case 4:
             System.out.println(num);
+            ctl.nextmenue();
             break;
+          case 5:
+            System.out.println("検索方法を選択してください\n1:前方一致\n2:後方一致\n3:曖昧検索");
+            try{
+              num = scan.nextInt();
+              if((num==1) || (num==2) || (num==3)) {ctl.namecheck(num);
+              }else{System.out.println("不正な数字です。");}
+            }catch(Exception e){System.out.println("error1");}
           case 99:
             end = 1;
             System.out.println("終了します。");
@@ -122,21 +132,24 @@ class Control{
     Scanner sc = new Scanner(System.in);
     System.out.println("今日の献立を入力してください");
     //String nms = sc.next();
-    /*byte[] nm =  sc.next().getBytes( StandardCharsets.UTF_8 );
-    System.out.println(nm);*/
-    String newmenue = new String( sc.nextLine() , StandardCharsets.UFT_8 );
+    /*byte[] nm =  sc.next().getBytes( StandardCharsets.UTF-8 );
+    System.out.println(nm);
+    String newmenue = new String( nm, StandardCharsets.UFT-8 );
+    */
+    String newmenue = sc.next();
 
-    //String newmenue = sc.next();
     System.out.println("テスト:"+newmenue);
     int delcheck = 0;
     int i;
     for(i = 14; i<menue.length; i++){
-      if(newmenue == menue[i]){
+      if(newmenue.equals(menue[i])){
+        System.out.println("same menue");
         menue[i]=null;
         delcheck=1;
       }
     }
     if(delcheck == 0){
+      System.out.println("no same");
       for(i = menue.length; i >1;i--){
         if(menue[i-2] != null){
           menue[i-1]=menue[i-2];
@@ -156,7 +169,7 @@ class Control{
       }
       menue[0]=newmenue;
     }
-    //change();
+    change();
   }//daymenue end
 
   void filehis(){
@@ -168,6 +181,98 @@ class Control{
     }
     System.out.println("===========================");
   }
+
+  void nextmenue(){
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("新しいメニューを入力してください");
+    String nxmenue = scanner.nextLine();
+    int i;
+    for(i = 0; i < menue.length ; i++){
+      if(menue[i]==null){
+        menue[i]=nxmenue;
+        break;
+      }
+    }
+    if(i>menue.length){
+      menue[i-1]=nxmenue;
+    }
+    change();
+
+  }//nextmenue end
+
+  void namecheck(int x){
+    System.out.println("検索する名前を入力してください");
+    Scanner sc = new Scanner(System.in);
+    String name = sc.nextLine();
+    String[] checklist = new String[28];
+    int i;
+    int l = 0;
+    switch(x){
+        case 1:
+          for(i = 0; i<menue.length;i++){
+            if(menue[i].startsWith(name)){
+              checklist[l]=menue[i];
+              l++;
+            }
+          }
+          break;
+
+        case 2:
+          for(i = 0; i<menue.length;i++){
+            if(menue[i].endsWith(name)){
+              checklist[l]=menue[i];
+              l++;
+            }
+          }
+          break;
+
+        case 3:
+          double[] score = new double[menue.length];
+          String[] key = name.split("");
+          for(i=0; i<key.length; i++){
+            for(l=0;l<key.length;l++){
+              if(key[i]!=null && key[l]!=null){
+                if(key[i].equals(key[l])){
+                  key[l]=null;
+                }
+              }
+            }
+          }
+          for(l=0;l<menue.length;l++){
+            score[l]=0;
+            for(i=0;i<key.length;i++){
+              if(key[i]!=null){
+                //Pattern p = Patturn.compile("^.*"+key[i]+".*$");
+                if(menue[l].matches("^.*"+key[i]+".*$")){
+                  score[l]=score[l]+1;
+                }
+              }else{
+                break;
+              }
+            }
+          }
+          double keylong = i;
+          l=0;
+          for(i=0;i<menue.length;i++){
+            score[i] = score[i] / keylong;
+            if(score[i] > 0.5){
+              checklist[l]=menue[i];
+              l++;
+            }
+          }
+          break;
+
+        default:
+          break;
+    }
+    System.out.println("========検索結果========");
+    for(i=0;i<checklist.length;i++){
+      if(checklist[i] != null){
+        System.out.println(checklist[i]);
+      }else{break;}
+    }
+    System.out.println("========================");
+  }//namecheck end
 
   void change(){
     //ファイルの書き込み
@@ -181,14 +286,11 @@ class Control{
           newtxt=(newtxt+"\n"+menue[i]);
         }
       }
-      pw.println("生米");
-      //System.out.println(newtxt);
+      pw.println(newtxt);
       pw.close();
     }catch(IOException e){
       e.printStackTrace();
     }
-
-
   }//change end
 
 }//操作クラス終了
